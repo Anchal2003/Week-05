a7b7b54720085e27f295e3ecca486a29fea22002
Anchal2003 <sahuanchal22@gmail.com>
2025-02-18 22:18:50 +0530

    [Anchal Sahu] Add Week-05 Welcome file.

---
+++ WelcomeToWeek5.java
@@ -0,0 +1,7 @@
+//Create a WelcomeToWeek5 to print the welcome statement
+public class WelcomeToWeek5 {
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to the Week-05!\"");
+    }
+
+}
