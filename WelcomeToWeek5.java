8a2518e3092a9a779b2710972f1ec4d1da7a8f8f
Anchal2003 <sahuanchal22@gmail.com>
2025-02-19 20:01:33 +0530

    [Anchal Sahu] Add Day02 JSON Handling practice solution.

--- WelcomeToWeek5.java
+++
@@ -1,7 +0,0 @@
-//Create a WelcomeToWeek5 to print the welcome statement
-public class WelcomeToWeek5 {
-    public static void main(String[] args) {
-        System.out.println("\"Welcome to the Week-05!\"");
-    }
-
-}
